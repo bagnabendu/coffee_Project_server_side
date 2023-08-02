98cc372a415bfde6064357e63bb82b3e046c38f0
bagnabendu <nabendubag8942@gmail.com>
2023-08-02 22:07:31 +0530

    first_server

---
+++ model/user.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    contact: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: Boolean,
+        default: true
+    },
+    isAdmin:{
+        type:Boolean,
+        default:false
+      },
+    // token:{
+    //     type:String,
+    //     default:''
+    // }
+}, { timestamps: true })
+const UserModel = new mongoose.model('user', UserSchema);
+module.exports = UserModel;
